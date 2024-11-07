d4ebb55c8facb7c7437c74339e145b7af3e448eb
Syanda Whisnu Pradana <84241286+Sansan-star@users.noreply.github.com>
2024-11-07 11:59:43 +0700

    Add files via upload
    The JavaScript file contains a quiz to test programming skills in JavaScript.

---
+++ kuis Mengarungi Lautan Sintaksis.js
@@ -0,0 +1,19 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+
+let firstName = 'syanda';
+let lastName = 'whisnu';
+let age = 25;
+let married = false;
+
+console.log("Tipe data firstName:", typeof firstName); 
+console.log("Tipe data lastName:", typeof lastName);   
+console.log("Tipe data age:", typeof age);             
+console.log("Tipe data married:", typeof married);     
